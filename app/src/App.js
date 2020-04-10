14bd68f79399811a36b6fde6fe784a56c1483782
patrickfeltes <pj6444@gmail.com>
2020-04-10 08:38:45 -0500

    Add instructor react crud table

--- app/src/App.js
+++ app/src/App.js
@@ -1,79 +1,12 @@
-import React, { useState, useEffect } from "react";
-import InstructorTable from "./components/InstructorTable";
-import AssignmentTable from "./components/AssignmentTable";
-import CourseTable from "./components/CourseTable";
-import AddInstructorForm from "./components/AddInstructorForm";
+import React from 'react';
+import InstructorTable from './components/InstructorTable'
 
-const App = () => {
-  const [instructors, setInstructors] = useState([]);
-  const [courses, setCourses] = useState([]);
-  const [assignments, setAssignments] = useState([]);
+const App = () => (
+  <InstructorTable>
 
-  //POST Payload to url with payload
-  const postPayload = (url, payload) => {
-    fetch(url, {
-      method: "POST",
-      body: payload,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  };
-
-  //GET method; only render once (at mounting time)
-  useEffect(() => {
-    fetch("/instructor").then((response) =>
-      response.json().then((data) => setInstructors(data))
-    );
-  }, []);
-
-  useEffect(() => {
-    fetch("/course").then((response) =>
-      response.json().then((data) => setCourses(data))
-    );
-  }, []);
-
-  useEffect(() => {
-    fetch("/assignment").then((response) =>
-      response.json().then((data) => setAssignments(data))
-    );
-  }, []);
-
-  return (
-    <div className="container">
-      <h1>CS Teaching Assignments Records</h1>
-      <div className="flex-row">
-        <div className="flex-large">
-          <h2>Add instructor</h2>
-          <AddInstructorForm postPayload={postPayload} />
-        </div>
-        <div className="flex-large">
-          <h2>View instructors</h2>
-          <InstructorTable instructors={instructors} />
-        </div>
-      </div>
-
-      <div className="flex-row">
-        <div className="flex-large">
-          <h2>Add assignment</h2>
-        </div>
-        <div className="flex-large">
-          <h2>View assignments</h2>
-          <AssignmentTable assignments={[assignments]} />
-        </div>
-      </div>
-
-      <div className="flex-row">
-        <div className="flex-large">
-          <h2>Add course</h2>
-        </div>
-        <div className="flex-large">
-          <h2>View courses</h2>
-          <CourseTable courses={courses} />
-        </div>
-      </div>
-    </div>
-  );
-};
-
+  </InstructorTable>
+);
+ 
+App.propTypes = {};
+ 
 export default App;
--- app/src/components/InstructorTable.js
+++ app/src/components/InstructorTable.js
@@ -1,38 +1,126 @@
 import React from "react";
-
-const InstructorTable = (props) => (
-  <table>
-    <thead>
-      <tr>
-        <th>instructorID</th>
-        <th>name</th>
-        <th>qualifications</th>
-        <th>atUIUC</th>
-        <th>semesterAvailable</th>
-      </tr>
-    </thead>
-    <tbody>
-      {props.instructors.length > 0 ? (
-        props.instructors.map((instructor) => (
-          <tr key={instructor.instructorID}>
-            <td>{instructor.instructorID}</td>
-            <td>{instructor.name}</td>
-            <td>{instructor.qualifications}</td>
-            <td>{instructor.atUIUC}</td>
-            <td>{instructor.semesterAvailable}</td>
-            <td>
-              <button className="button muted-button">Edit</button>
-              <button className="button muted-button">Delete</button>
-            </td>
-          </tr>
-        ))
-      ) : (
-        <tr>
-          <td colSpan={3}>No users</td>
-        </tr>
-      )}
-    </tbody>
-  </table>
+import CRUDTable,
+{
+  Fields,
+  Field,
+  CreateForm,
+  UpdateForm,
+  DeleteForm,
+} from 'react-crud-table';
+ 
+const instructorService = {
+  fetchItems: (payload) => {
+    return fetch('/instructor').then(response => {
+      return response.json();
+    });
+  },
+  create: (instructor) => {
+    return fetch('/instructor', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(instructor)
+    });
+  },
+  update: (instructor) => {
+    return fetch('/instructor', {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(instructor)
+    });
+  },
+  delete: (data) => {
+    return fetch('/instructor/' + data.instructorID, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+  },
+};
+ 
+const styles = {
+  container: { margin: 'auto', width: 'fit-content' },
+};
+ 
+const InstructorTable = () => (
+  <div style={styles.container}>
+    <CRUDTable
+      caption="Instructors"
+      fetchItems={payload => instructorService.fetchItems(payload)}
+    >
+      <Fields>
+        <Field
+          name="instructorID"
+          label="instructorID"
+          hideInCreateForm
+          hideInUpdateForm
+        />
+        <Field
+          name="name"
+          label="name"
+        />
+        <Field
+          name="qualifications"
+          label="qualifications"
+        />
+        <Field
+          name="atUIUC"
+          label="atUIUC"
+        />
+        <Field
+          name="semesterAvailable"
+          label="semesterAvailable"
+        />
+      </Fields>
+      <CreateForm
+        title="Instructor Creation"
+        message="Create a new instructor!"
+        trigger="Create Instructor"
+        onSubmit={task => instructorService.create(task)}
+        submitText="Create"
+        validate={(values) => {
+          const errors = {};
+ 
+          return errors;
+        }}
+      />
+ 
+      <UpdateForm
+        title="Instructor Update Process"
+        message="Update instructor"
+        trigger="Update"
+        onSubmit={task => instructorService.update(task)}
+        submitText="Update"
+        validate={(values) => {
+          const errors = {};
+ 
+          return errors;
+        }}
+      />
+ 
+      <DeleteForm
+        title="Instructor Delete Process"
+        message="Are you sure you want to delete the instructor?"
+        trigger="Delete"
+        onSubmit={task => instructorService.delete(task)}
+        submitText="Delete"
+        validate={(values) => {
+          const errors = {};
+          
+          return errors;
+        }}
+      />
+    </CRUDTable>
+  </div>
 );
+
+InstructorTable.propTypes = {};
 
 export default InstructorTable;
