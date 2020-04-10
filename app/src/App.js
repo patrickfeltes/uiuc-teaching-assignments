c8e3b607bbb56d62297741babd4a51637d044ef2
roshanxzy <roshanxzy@gmail.com>
2020-04-10 04:49:38 -0500

    Finished POST and GET

--- app/src/App.js
+++ app/src/App.js
@@ -2,29 +2,42 @@ import React, { useState, useEffect } from "react";
 import InstructorTable from "./components/InstructorTable";
 import AssignmentTable from "./components/AssignmentTable";
 import CourseTable from "./components/CourseTable";
+import AddInstructorForm from "./components/AddInstructorForm";
 
 const App = () => {
   const [instructors, setInstructors] = useState([]);
   const [courses, setCourses] = useState([]);
   const [assignments, setAssignments] = useState([]);
 
+  //POST Payload to url with payload
+  const postPayload = (url, payload) => {
+    fetch(url, {
+      method: "POST",
+      body: payload,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
+  //GET method; only render once (at mounting time)
   useEffect(() => {
     fetch("/instructor").then((response) =>
       response.json().then((data) => setInstructors(data))
     );
-  });
+  }, []);
 
-  useEffect(() => {
-    fetch("/course").then((response) =>
-      response.json().then((data) => setCourses(data))
-    );
-  });
+  // useEffect(() => {
+  //   fetch("/course").then((response) =>
+  //     response.json().then((data) => setCourses(data))
+  //   );
+  // }, []);
 
-  useEffect(() => {
-    fetch("/assignment").then((response) =>
-      response.json().then((data) => setAssignments(data))
-    );
-  });
+  // useEffect(() => {
+  //   fetch("/assignment").then((response) =>
+  //     response.json().then((data) => setAssignments(data))
+  //   );
+  // }, []);
 
   return (
     <div className="container">
@@ -32,6 +45,7 @@ const App = () => {
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add instructor</h2>
+          <AddInstructorForm postPayload={postPayload} />
         </div>
         <div className="flex-large">
           <h2>View instructors</h2>
@@ -45,7 +59,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View assignments</h2>
-          <AssignmentTable assignments={assignments} />
+          <AssignmentTable assignments={[assignments]} />
         </div>
       </div>
 
