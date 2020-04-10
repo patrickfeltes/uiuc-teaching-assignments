e3ed73f13d414d3f87ca53ed4b1e4a9ec874d40c
patrickfeltes <pj6444@gmail.com>
2020-04-10 12:49:38 -0500

    Add css

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,8 @@ import InstructorTable from './components/InstructorTable'
 import AssignmentTable from './components/AssignmentTable'
 import CourseTable from './components/CourseTable'
 
+import './index.css'
+
 const App = () => (
   <div>
     <InstructorTable />
